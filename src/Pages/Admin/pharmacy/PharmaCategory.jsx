2907e7fbbd00af89e98e78ad0f4050ca39d7d217
sambhubaburaj <sambhubaburaj513@gmail.com>
2024-01-21 11:07:34 +0530

    morning pull

--- src/Pages/Admin/pharmacy/PharmaCategory.jsx
+++ src/Pages/Admin/pharmacy/PharmaCategory.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { pharmacyNav } from "../../../Redux/Features/NavbarSlice";
-
+import CatCard from "../../../components/CatCard";
+import lungsimg from "../../../assets/images/3d-fluency-lungs.png";
+import heartimg from "../../../assets/images/heart.png";
 export default function PharmaCategory() {
   const [categoryMenu, setCategoryMenu] = useState(true);
 
@@ -11,6 +13,11 @@ const changeCategory=()=>
 {
     setCategoryMenu(!categoryMenu)
 }
+const abc = { name: "Pulmonology", image: lungsimg };
+const ab = { name: "Hepatology", image: heartimg };
+const editCat = (data) => {
+  console.log(data);
+};
 
 
   useEffect(() => {
@@ -27,7 +34,7 @@ const changeCategory=()=>
             categoryMenu && "font-bold underline"
           }  text-xl  underline-offset-8 decoration-4 cursor-pointer`}
         >
-          Category
+          Categories
         </p>
         <p
           onClick={() => {
@@ -44,10 +51,19 @@ const changeCategory=()=>
       </div>
 
       <div>
-        <h4 className="text-4xl font-semibold ">{categoryMenu?"category":"sub category"}</h4>
-        <p className="p-2">5 categories</p>
+        <h4 className="text-4xl font-semibold p-4 ">{categoryMenu?"Categories":"sub Categories"}</h4>
+        <p className="p-2 pl-3 text-gray-600 font-semibold">5 categories</p>
       </div>
+<div>
 
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-4 mt-6">
+        <CatCard data={ab} callback={editCat} />
+        <CatCard data={abc} callback={editCat} />
+        <CatCard data={abc} callback={editCat} />
+        <CatCard data={abc} callback={editCat} />
+        <CatCard data={abc} callback={editCat} />
+      </div>
+</div>
       
     </div>
   );
