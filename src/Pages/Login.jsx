0ca6a769737962469e276306eb32f02651cb3b51
brijin07 <brijingeorge07@gmail.com>
2024-01-21 21:09:11 -0800

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -187,12 +187,12 @@ const Login = () => {
                 for="floating_outlined"
                 class="absolute text-sm text-gray-500   duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-100 px-2 peer-focus:px-2 peer-focus:text-blue-600    peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
               >
-                Floating outlined
-              </label>
+Email Adress              </label>
             </div>
 
             <div class="relative">
               <input
+              
                 name="password"
                 type="password"
                 id="floating_outlined1"
@@ -207,8 +207,7 @@ const Login = () => {
                 for="floating_outlined1"
                 class="absolute text-sm text-gray-500   duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-100 px-2 peer-focus:px-2 peer-focus:text-blue-600    peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
               >
-                Floating outlined
-              </label>
+Password              </label>
             </div>
 
             <div>
