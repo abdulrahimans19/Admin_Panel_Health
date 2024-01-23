67751c5b41d3cdb7b29ca659a45f29c619bc0ba2
brijin07 <brijingeorge07@gmail.com>
2024-01-23 07:39:02 -0800

    forgot

--- src/API/ApiCall.js
+++ src/API/ApiCall.js
@@ -10,6 +10,18 @@ export const LoginUserdata = async (data) => {
   return await Instance.post("/auth/sign-in", data);
 };
 
+export const SignupUserdata = async(data) =>{
+  return await Instance.post("/auth/doctor/sign-up", data);
+};
+
+export const DoctorForgotdata = async(data) =>{
+  return await Instance.post("/auth/doctor/forgot-password", data);
+} ;
+
+
+
+
+
 
 
 
