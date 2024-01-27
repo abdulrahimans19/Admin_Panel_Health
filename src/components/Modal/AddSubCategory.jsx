c194a797fd210af775ee9c556b8b60b8ae16ee76
sambhubaburaj <sambhubaburaj513@gmail.com>
2024-01-27 12:15:59 +0530

    working on addming product api

--- src/components/Modal/AddSubCategory.jsx
+++ src/components/Modal/AddSubCategory.jsx
@@ -1,39 +1,34 @@
 // src/Modal.js
 
-import React, { useState } from 'react';
-import { createSubCategory } from '../../API/ApiCall';
+import React, { useState } from "react";
+import { createSubCategory } from "../../API/ApiCall";
 
-const AddSubCategoryModal = ({ onClose,displayData }) => {
+const AddSubCategoryModal = ({ onClose, displayData }) => {
   const [subCategories, setSubCategories] = useState();
-const [subCatName, setSubCatName] = useState('')
+  const [subCatName, setSubCatName] = useState("");
   const handleOptionChange = (e) => {
     const selectedOption = e.target.value;
     setSubCategories(selectedOption);
   };
 
-
   const handleSubmit = () => {
     // Handle the form submission logic here
-    console.log(subCatName
-      );
-    console.log('SubCategories:', subCategories);
-console.log();
+    console.log(subCatName);
+    console.log("SubCategories:", subCategories);
+    console.log();
     // Close the modal
-const wholedata={
-  title:subCatName,
-  main_category_id:subCategories
-}
-    createSubCategory(wholedata).then(data=>
-      {
+    const wholedata = {
+      title: subCatName,
+      main_category_id: subCategories,
+    };
+    createSubCategory(wholedata)
+      .then((data) => {
         onClose();
-
-      }).catch((err)=>
-      {
+      })
+      .catch((err) => {
         console.log(err);
         onClose();
-
-      })
-
+      });
   };
 
   return (
@@ -42,7 +37,10 @@ const wholedata={
       <div className="bg-white p-8 max-w-md w-full rounded-lg shadow-lg z-50">
         <h2 className="text-2xl font-bold mb-4">Add Subcategory</h2>
 
-        <label htmlFor="dropdown" className="block text-sm font-medium text-gray-700">
+        <label
+          htmlFor="dropdown"
+          className="block text-sm font-medium text-gray-700"
+        >
           Select category:
         </label>
         <select
@@ -51,19 +49,13 @@ const wholedata={
           className="mt-1 p-2 border rounded-md w-full"
           onChange={handleOptionChange}
         >
-          {
-displayData.map((data)=>
-{
-  return (  <option value={data._id}>{data.title}</option>)
-})
-
-          }
-        
-      
+          {displayData.map((data) => {
+            return <option value={data._id}>{data.title}</option>;
+          })}
         </select>
-        <label  className="block text-sm font-medium text-gray-700 mt-2">
-              Sub category:
-            </label>
+        <label className="block text-sm font-medium text-gray-700 mt-2">
+          Sub category:
+        </label>
         {/* {subCategories.map((subCategory, index) => (
           <div key={index} className="mb-4">
            
@@ -77,17 +69,15 @@ displayData.map((data)=>
       
           </div>
         ))} */}
-      <input
-              type="text"
-              name="subcat"
-              onChange={(e)=>
-              {
-                setSubCatName(e.target.value)
-              }}
-              className="mt-1 p-2 border rounded-md w-full"
-              // onChange={(e) => handleInputChange(index, e)}
-            />
-   
+        <input
+          type="text"
+          name="subcat"
+          onChange={(e) => {
+            setSubCatName(e.target.value);
+          }}
+          className="mt-1 p-2 border rounded-md w-full"
+          // onChange={(e) => handleInputChange(index, e)}
+        />
 
         <div className="flex justify-end mt-3">
           <button
@@ -98,10 +88,8 @@ displayData.map((data)=>
           </button>
           <button
             className="ml-2 text-red-500 bg-red-200  px-4 py-2 rounded-md"
-            onClick={()=>
-            {
-        onClose();
-
+            onClick={() => {
+              onClose();
             }}
           >
             Cancel
